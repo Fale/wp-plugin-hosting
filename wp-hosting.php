04f4bb98d33e900440d9dd95f354ff73a5fecec5
Fabio Alessandro Locati <flocati@grimp.eu>
2010-11-09 10:23:47 +0100

    Add the config file

---
+++ wp-hosting.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WP-Hosting
+Plugin URI: http://git.grimp.eu/projects/wp-hosting
+Description: This plugin will allow you to manage hosting plans, hosting comparison etc.
+Version: 0.1
+Author: Fabio Alessandro Locati
+Author URI: http://grimp.eu
+License: GPL2
+*/
+?>
