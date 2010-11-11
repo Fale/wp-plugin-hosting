5a3b28d0d66374acd981e984ef02e51680d3923f
Fabio Alessandro Locati <flocati@grimp.eu>
2010-11-11 18:04:37 +0100

    Add a testing SQL SELECT query

--- wp-hosting_admin.php
+++ wp-hosting_admin.php
@@ -8,8 +8,14 @@ function wp_hosting_options() {
   if (!current_user_can('manage_options'))  {
     wp_die( __('You do not have sufficient permissions to access this page.') );
   }
+  
+  global $wpdb;
+  
   echo '<div class="wrap">';
+  echo '<h1>View hosting plans</h1>';
   echo '<p>Here is where the form would go if I actually had options.</p>';
+  echo $wpdb->query("SELECT * FROM asd WHERE id > 0");
+  echo $wpdb->get_var("SELECT name FROM asd");
   echo '</div>';
 }
 
